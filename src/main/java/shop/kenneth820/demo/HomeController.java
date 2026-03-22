86382e3d4e0872f7dd1d9366dfb2b464af0eefb1
kenneth <kenneth820@naver.com>
2026-03-22 04:25:21 +0000

    modified:   HomeController.java

--- src/main/java/shop/kenneth820/demo/HomeController.java
+++ src/main/java/shop/kenneth820/demo/HomeController.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 package shop.kenneth820.demo;  // 패키지명 꼭 맞춰야 함
-=======
-package shop.kenneth820.demo;
->>>>>>> a54a166 (Implement real CI: Java setup and Maven build)
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-<<<<<<< HEAD
     @GetMapping("/")  // 루트 경로 요청 처리
     public String home() {
-        return "index";  // templates/index.html 반환
-=======
-    @GetMapping("/")
-    public String home() {
         return "index";   // resources/templates/index.html 연결
->>>>>>> a54a166 (Implement real CI: Java setup and Maven build)
     }
 }
